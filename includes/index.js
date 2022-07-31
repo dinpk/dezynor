0dbde6af44b2894ea9bec04410a964a1f05a1b26
Desktop <ndin.pk@gmail.com>
2022-07-31 20:55:29 +0500

    Folders can be deleted now

--- includes/index.js
+++ includes/index.js
@@ -51,12 +51,47 @@ async function addFolder() {
 
 }
 
+async function deleteFolder(folder_name) {
+	
+	let all_designs = await idbGetAllItems("dezynor_designs");
+	for (i = 0; i < all_designs.length; i++) {
+		let design_key = all_designs[i].design_key;
+		let design_data = all_designs[i].value;
+		if (design_key.split("|")[1] == folder_name) {
+			alert("Folder '" + folder_name + "' is not empty");
+			return;
+		}
+	}
+			
+	if (confirm("Do you really want to delete folder '" + folder_name + "'?")) {
+		
+		let folders = await idbGetItem("dezynor_settings", "folders");
+		let index = folders.indexOf(folder_name);
+		if (index > -1) {
+			folders.splice(index, 1);
+			await idbPutItem("dezynor_settings", {setting_key:"folders", value:folders});
+			loadFolders();
+			showMessage("Folder '" + folder_name + "' deleted successfully", "Red");
+		}
+	}
+
+}
+
 async function renameFolder(folder_name) {
+
+	// add folder
+	
 	let new_folder_name = prompt("Enter new folder name");
 	if (!new_folder_name || new_folder_name.trim().length == 0) return;
 
 	let folders = await idbGetItem("dezynor_settings", "folders");
-	let index = folders.indexOf(folder_name);
+	
+	let index = folders.indexOf(new_folder_name);
+	if (index > -1) {
+		alert("Folder '" + new_folder_name + "' already exists");
+		return;
+	}
+	index = folders.indexOf(folder_name);
 	if (index > -1) {
 		folders.splice(index, 1);
 		folders.push(new_folder_name);
@@ -64,11 +99,26 @@ async function renameFolder(folder_name) {
 		alert("'" + folder_name + "' does not exist");
 		return;
 	}
-	idbPutItem("dezynor_settings", {setting_key:"folders", value:folders});
+	await idbPutItem("dezynor_settings", {setting_key:"folders", value:folders});
 	loadFolders();
 	document.getElementById("new_folder").focus();
-	showMessage("'" + folder_name + "' renamed to '" + new_folder_name + "'", "Green");
 
+	// update design keys
+	
+	let all_designs = await idbGetAllItems("dezynor_designs");
+	for (i = 0; i < all_designs.length; i++) {
+		let design_key = all_designs[i].design_key;
+		let design_data = all_designs[i].value;
+		if (design_key.split("|")[1] == folder_name) {
+			await idbRemoveItem("dezynor_designs", design_key);
+			let new_design_key = design_key.split("|")[0] + "|" + new_folder_name;
+			await idbPutItem("dezynor_designs", {design_key:new_design_key, value:design_data});
+		}
+	}
+
+	showMessage("Folder '" + folder_name + "' renamed to '" + new_folder_name + "'", "Green");
+	showFolderDezyns(new_folder_name);
+	
 }
 
 async function loadFolders() {
@@ -76,7 +126,8 @@ async function loadFolders() {
 	folders.sort();
 	let folders_html = "";
 	for (let i = 0; i < folders.length; i++) {
-		folders_html = folders_html + "<div><span onclick=\"renameFolder('" + folders[i].trim() + "');\">" + "R</span> <a onclick=\"showFolderDezyns('" + folders[i].trim() + "');\">" + folders[i].trim() + "</a></div>";
+		let folder_name = folders[i].trim();
+		folders_html = folders_html + "<div><a onclick=\"showFolderDezyns('" + folder_name + "');\">" + folder_name + "</a></div>";
 	}
 	document.getElementById("folders").innerHTML = folders_html;		
 }
@@ -107,7 +158,10 @@ async function showFolderDezyns(folder) {
 
 	let message = await hideMessage(); // await
 	document.getElementById("message").innerHTML = message;
-	document.getElementById("folder_label").innerHTML = "<h2>" + folder + "</h2>";
+	let folder_label = "<b>" + folder + "</b> ";
+	folder_label = folder_label + " <span id='delete_folder' onclick=\"deleteFolder('" + folder + "');\" title='Delete this folder'>" + "D</span>";
+	folder_label = folder_label + " <span id='rename_folder' onclick=\"renameFolder('" + folder + "');\" title='Rename this folder'>" + "R</span>";	
+	document.getElementById("folder_label").innerHTML = "<div>" + folder_label + "</div>";
 
 }
 
