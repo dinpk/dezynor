c6e5a6dfb42ab8a79aad215b32b8daeed91bd926
Dinpk <ndin.pk@gmail.com>
2022-09-02 23:54:34 +0500

    Improved upgrade procedure

--- includes/indexeddb.js
+++ includes/indexeddb.js
@@ -1,38 +1,42 @@
 
 
 let db_name = "dezynordb";
-let db_version = "5";
+let db_version = "1";
 let connection = indexedDB.open(db_name, db_version);
 
 connection.onupgradeneeded = function(e) {
 	let db = connection.result;
-	if (!db.objectStoreNames.contains("dezynor_designs")) {
-		let dezynor_designs = db.createObjectStore("dezynor_designs", {keyPath: "design_key"});
-		let dezynor_settings = db.createObjectStore("dezynor_settings", {keyPath: "setting_key"});
-		let dezynor_images = db.createObjectStore("dezynor_images", {keyPath: "image_key"});
-		dezynor_settings.add({setting_key: "folders", value: ["default"]});
-		dezynor_settings.add({setting_key: "fonts", value: ["Anton|Google", "Smooch|Google", "Arial|Installed", "Verdana|Installed"]});
-		localStorage.setItem("copied_section", "");
-		localStorage.setItem("show_background_image", "1");
-		localStorage.setItem("duplicate_offset_x", "10");
-		localStorage.setItem("duplicate_offset_y", "0");
-		localStorage.setItem("move_offset", "2");
-		localStorage.setItem("rotate_offset", "1");
-		localStorage.setItem("font_size_change", "3");
-		localStorage.setItem("line_height_change", "3");
-		localStorage.setItem("word_spacing_change", "1");
-		localStorage.setItem("max_upload_width", "1200");
-		localStorage.setItem("max_upload_height", "1200");
-		localStorage.setItem("current_folder", "default");
+    console.log("DB updated from version " + e.oldVersion +  " to " + e.newVersion);
+	switch (e.oldVersion) {
+		case 0:
+			console.log("Case 0: Adding database, stores and settings");
+			let dezynor_designs = db.createObjectStore("dezynor_designs", {keyPath: "design_key"});
+			dezynor_designs.createIndex('createdIndex', 'created', { unique: false });
+			dezynor_designs.createIndex('modifiedIndex', 'modified', { unique: false });		
+			let dezynor_settings = db.createObjectStore("dezynor_settings", {keyPath: "setting_key"});
+			let dezynor_images = db.createObjectStore("dezynor_images", {keyPath: "image_key"});
+			dezynor_settings.add({setting_key: "folders", value: ["default"]});
+			dezynor_settings.add({setting_key: "fonts", value: ["Anton|Google", "Smooch|Google", "Arial|Installed", "Verdana|Installed"]});
+			localStorage.setItem("copied_section", "");
+			localStorage.setItem("show_background_image", "1");
+			localStorage.setItem("duplicate_offset_x", "10");
+			localStorage.setItem("duplicate_offset_y", "0");
+			localStorage.setItem("move_offset", "2");
+			localStorage.setItem("rotate_offset", "1");
+			localStorage.setItem("font_size_change", "3");
+			localStorage.setItem("line_height_change", "3");
+			localStorage.setItem("word_spacing_change", "1");
+			localStorage.setItem("max_upload_width", "1200");
+			localStorage.setItem("max_upload_height", "1200");
+			localStorage.setItem("current_folder", "default");
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
 	}
-	
-	    console.log("DB updated from version " + e.oldVersion +  " to " + e.newVersion);
 
-	if (e.oldVersion == 5) {
-		// console.log("changes for next version");
-	} else if (e.oldVersion == 6) {
 
-	}
 };
 
 connection.onerror = function(e) {
@@ -109,11 +113,13 @@ async function idbKeyExists(store, key) {
 async function idbGetAllItems(store) {
 	let db = connection.result;
 	let transaction = db.transaction(store, "readonly");
-	store = transaction.objectStore(store);
+	object_store = transaction.objectStore(store);
+	// let idx = object_store.index("modifiedIndex");
     return new Promise(function(resolve, reject) {
-		let all_items = store.getAll();
+		let all_items = object_store.getAll();
 		all_items.onsuccess = function(e) {
 			resolve(all_items.result);
+			console.log(all_items.result);
 		}
     });	
 }
